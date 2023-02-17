9c22e27251646a2be4c3edcf0c1e0bde6fe9d010
Hariharan rajendran <hariharan.r@oxsoftwares.com>
2023-02-17 11:00:47 +0530

    first commit

---
+++ new.py
@@ -0,0 +1,27 @@
+"""
+arr = [25,36,47,89,5,2,3]
+max = arr[0]
+size = len(arr)
+for i in range (1, size):
+    if arr[i] < max:
+        max=arr[i]
+
+
+print(max)
+"""
+"""
+# for loop Explaniation
+
+n=10
+for i in range(n):
+    print("hi")
+"""
+# if else condition
+
+n = int(input("ENter the number to find even or odd number:"))
+if n%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+    
+    
