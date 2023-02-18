bb68b0c3855d44a18bf5f218813feca8e836f9d1
Haris <itzharis@gmail.com>
2023-02-18 11:43:52 +0530

    new

---
+++ Lists/testtutorial.py
@@ -0,0 +1,4 @@
+lst=[1,2,3,4]
+lst.append([70])#last add panum
+lst.extend([70]) #extend [] kulla than varum
+print (lst)
--- loops/jumpstatement_continue.py
+++ loops/jumpstatement_continue.py
@@ -1,4 +1,4 @@
-for i in range(10):
+for i in range(10): # 0 la start agi end la -1 ah irukum
     if i==5:
         continue
     print(i)
